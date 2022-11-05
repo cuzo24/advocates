928faa3b47a418a14f8df6896f7bfaab5ceffca4
Carlos Zamora <czamora5@ucol.mx>
2022-11-05 17:09:57 -0600

    feat: add responsive typography, font files and breakpoints

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,21 @@
+export const sizes = {
+  'xx': '11rem',
+  'xs': '23.5rem',
+  'sm': '36rem',
+  'md': '48rem',
+  'lg': '60rem',
+  'xl': '80rem',
+  '2xl': '100rem',
+  '3xl': '120rem'
+};
+
+export const breakpoints = {
+  'xx': `min-width: ${sizes['xx']}`,
+  'xs': `min-width: ${sizes['xs']}`,
+  'sm': `min-width: ${sizes['sm']}`,
+  'md': `min-width: ${sizes['md']}`,
+  'lg': `min-width: ${sizes['lg']}`,
+  'xl': `min-width: ${sizes['xl']}`,
+  '2xl': `min-width: ${sizes['2xl']}`,
+  '3xl': `min-width: ${sizes['3xl']}`
+};
